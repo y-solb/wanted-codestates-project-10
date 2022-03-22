ddbec68766445f5cbfe4ef0f9de02560b6661331
y-solb <ysolb99@gmail.com>
2022-03-22 14:15:20 +0900

    Design: ThemeProvider 추가

---
+++ src/styles/theme.js
@@ -0,0 +1,14 @@
+const colors = {
+  black: '#000000',
+  white: '#FFFFFF',
+  skyBlue: '#CAE9FF',
+  blue: '#007BE9',
+  gray: '#6A737B',
+  lightGray: '#f4f4f4',
+};
+
+const theme = {
+  colors,
+};
+
+export default theme;
